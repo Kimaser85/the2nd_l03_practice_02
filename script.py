afb8b06a1c919d493956de622b38b5b96ed8a075
kimaser85 <kimaser85@gmail.com>
2021-12-20 12:59:59 +0300

    V2

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint 
 
 BASE_URL = "https://api.giphy.com/v1/stickers/random"
 params = dict(
@@ -11,9 +10,7 @@ params = dict(
         )
 response = requests.get(BASE_URL, params=params)
 print(response.ok)
-pprint(response.json())#)['data'])#['id'])#['fixed_height_small'])
- #'_url'])
-# gif_url = response.json()['data']['fixed_height_small_url']
-# resp_gif = requests.get(gif_url)
-# with open("test.gif", "wb") as f:
-#     f.write(resp_gif.content)
+gif_url = response.json()['data']['images']['fixed_height_small']["url"]
+resp_gif = requests.get(gif_url)
+with open("test.gif", "wb") as f:
+    f.write(resp_gif.content)
