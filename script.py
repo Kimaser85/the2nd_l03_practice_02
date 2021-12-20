3147b95d7ad1d852da1dcc856a104539ee93dab9
kimaser85 <kimaser85@gmail.com>
2021-12-20 16:13:47 +0300

    Rebyild py and get 3- gif

--- script.py
+++ script.py
@@ -1,20 +1,17 @@
 import requests
-
+gif_list = ["fail", "people", "lucky"]
 BASE_URL = "https://api.giphy.com/v1/stickers/random"
-params = dict(
+for gif in gif_list:
+    params = dict(
         api_key="dc6zaTOxFJmzC",
-        tag="python",
-        limit=1,
+        tag="lucky",
         rating="g",
+        limit=1,
         fmt='json'
         )
-response = requests.get(BASE_URL, params=params)
-print(response.ok)
-gif_url = response.json()['data']['images']['fixed_height_small']["url"]
-resp_gif = requests.get(gif_url)
-with open("test.gif", "wb") as f:
-    f.write(resp_gif.content)
-
-
-
-
+    response = requests.get(BASE_URL, params=params)
+    print(response.ok)
+    gif_url = response.json()['data']["images"]['fixed_height_small']['url']
+    resp_gif = requests.get(gif_url)
+    with open(gif+".gif", "wb") as f:
+        f.write(resp_gif.content)
